3130fea3dd7ddfdc8465e09c7be0359b0cf2d167
Toktam <ttmohebbi@gmail.com>
2020-08-31 22:14:28 +0000

    created HTTP Server with some messages on its pages

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function(request, response) {
-  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+  console.log('In requestHandler'); // NEW LINE
+  // response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+  if (request.url === "/" ) {
+  response.end("Welcome!");
+} else if (request.url === "/urls") {
+  response.end("google.com");
+} else {
+  response.statusCode = 404;
+  response.end("404 Page Not Found");
+}
 };
 
+
+
 const server = http.createServer(requestHandler);
+console.log('Server created'); // NEW LINE
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
 
+console.log('Last line (after .listen call)'); // NEW LINE
